d4650b5d0bf6f2c022bd4575c3683d8bec63de1c
behroozmrd47 <37091119+behroozmrd47@users.noreply.github.com>
2018-03-26 20:46:18 -0500

    Update part-1-1of3.R

--- part-1-1of3.R
+++ part-1-1of3.R
@@ -13,3 +13,93 @@ plot(lm.lat, which = c(1))
 summary(lm.lat)$r.squared
 plot(lm.lon, which = c(1))
 summary(lm.lon)$r.squared
+
+
+################# REGULRIZED LINEAR REGRESSION LATITUDE ################# 
+#Unregulrized linear regression of latitude against 116 other features
+cv.lat.unreg = cv.glmnet(x_matrix,y_lat,alpha = 0, lambda=c(0,0.1), nfolds=10)
+plot(cv.lat.unreg)
+title("Unregulrized - Latitude")
+min(cv.lat.unreg[["lambda"]])
+cv.lat.unreg[["cvm"]][which.min(cv.lat.unreg[["lambda"]])]
+
+#Regulrized ridge linear regression of latitude against 116 other features
+cv.lat.reg = cv.glmnet(x_matrix,y_lat,alpha = 0, nfolds=10)
+plot(cv.lat.reg)
+title("Regulrized Ridge - Latitude")
+min(cv.lat.reg[["lambda"]])
+cv.lat.reg[["cvm"]][which.min(cv.lat.reg[["lambda"]])]
+
+
+#Regulrized lasso linear regression of latitude against 116 other features
+cv.lat.reg = cv.glmnet(x_matrix,y_lat,alpha = 1, nfolds=10)
+plot(cv.lat.reg)
+title("Regulrized Lasso - Latitude")
+min(cv.lat.reg[["lambda"]])
+cv.lat.reg[["cvm"]][which.min(cv.lat.reg[["lambda"]])]
+
+#Regulrized elastic net linear regression of latitude with alpha of 0.25
+cv.lat.reg = cv.glmnet(x_matrix,y_lat,alpha = .25, nfolds=10)
+plot(cv.lat.reg)
+title("Regulrized Elastic Net Alpha=0.25 - Latitude")
+min(cv.lat.reg[["lambda"]])
+cv.lat.reg[["cvm"]][which.min(cv.lat.reg[["lambda"]])]
+
+#Regulrized elastic net linear regression of latitude with alpha of 0.50
+cv.lat.reg = cv.glmnet(x_matrix,y_lat,alpha = .50, nfolds=10)
+plot(cv.lat.reg)
+title("Regulrized Elastic Net Alpha=0.50 - Latitude")
+min(cv.lat.reg[["lambda"]])
+cv.lat.reg[["cvm"]][which.min(cv.lat.reg[["lambda"]])]
+
+#Regulrized elastic net linear regression of latitude with alpha of 0.75
+cv.lat.reg = cv.glmnet(x_matrix,y_lat,alpha = .75, nfolds=10)
+plot(cv.lat.reg)
+title("Regulrized Elastic Net Alpha=0.75 - Latitude")
+min(cv.lat.reg[["lambda"]])
+cv.lat.reg[["cvm"]][which.min(cv.lat.reg[["lambda"]])]
+
+
+################# REGULRIZED LINEAR REGRESSION LONGTITUDE ################# 
+
+#Unregulrized linear regression of longtitude against 116 other features
+cv.long.unreg = cv.glmnet(x_matrix,y_long,alpha = 0, lambda=c(0,0.1), nfolds=10)
+plot(cv.long.unreg)
+title("Unregulrized - Longtitude")
+min(cv.long.unreg[["lambda"]])
+cv.long.unreg[["cvm"]][which.min(cv.long.unreg[["lambda"]])]
+
+#Regulrized ridge linear regression of Longtitude against 116 other features
+cv.long.reg = cv.glmnet(x_matrix,y_long,alpha = 0, nfolds=10)
+plot(cv.long.reg)
+title("Regulrized Ridge - Longtitude")
+min(cv.long.reg[["lambda"]])
+cv.long.reg[["cvm"]][which.min(cv.long.reg[["lambda"]])]
+
+#Regulrized lasso linear regression of Longtitude against 116 other features
+cv.long.reg = cv.glmnet(x_matrix,y_long,alpha = 1, nfolds=10)
+plot(cv.long.reg)
+title("Regulrized Lasso - Longtitude")
+min(cv.long.reg[["lambda"]])
+cv.long.reg[["cvm"]][which.min(cv.long.reg[["lambda"]])]
+
+#Regulrized elastic net linear regression of Longtitude with alpha of 0.25
+cv.long.reg = cv.glmnet(x_matrix,y_long,alpha = .25, nfolds=10)
+plot(cv.long.reg)
+title("Regulrized Elastic Net Alpha=0.25 - Longtitude")
+min(cv.long.reg[["lambda"]])
+cv.long.reg[["cvm"]][which.min(cv.long.reg[["lambda"]])]
+
+#Regulrized elastic net linear regression of Longtitude with alpha of 0.50
+cv.long.reg = cv.glmnet(x_matrix,y_long,alpha = .50, nfolds=10)
+plot(cv.long.reg)
+title("Regulrized Elastic Net Alpha=0.50 - Longtitude")
+min(cv.long.reg[["lambda"]])
+cv.long.reg[["cvm"]][which.min(cv.long.reg[["lambda"]])]
+
+#Regulrized elastic net linear regression of Longtitude with alpha of 0.75
+cv.long.reg = cv.glmnet(x_matrix,y_long,alpha = .75, nfolds=10)
+plot(cv.long.reg)
+title("Regulrized Elastic Net Alpha=0.75 - Longtitude")
+min(cv.long.reg[["lambda"]])
+cv.long.reg[["cvm"]][which.min(cv.long.reg[["lambda"]])]
